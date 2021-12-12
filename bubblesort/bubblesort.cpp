a69d556616b90da078678c89a983fc5317fb380d
Carson Goins <carsongoins20@gmail.com>
2021-12-12 00:11:19 -0500

    update

--- bubblesort/bubblesort.cpp
+++ bubblesort/bubblesort.cpp
@@ -4,72 +4,77 @@
 #include <fcntl.h>
 #include <iostream>
 #include <unistd.h>
-#include "omploop.hpp"
-
+#include <omp.h>
 #include <chrono>
 
-#ifdef cplusplus
+using namespace std;
+
+#ifdef __cplusplus
 extern "C" {
 #endif
 
   void generateMergeSortData (int* arr, size_t n);
   void checkMergeSortResult (const int* arr, size_t n);
 
-#ifdef cplusplus
+#ifdef __cplusplus
 }
 #endif
 
+
 int main (int argc, char* argv[]) {
 
-  // start timer
-  auto start = std::chrono::system_clock::now();
 
+  //forces openmp to create the threads beforehand
+#pragma omp parallel
+  {
+    int fd = open (argv[0], O_RDONLY);
+    if (fd != -1) {
+      close (fd);
+    }
+    else {
+      std::cerr<<"something is amiss"<<std::endl;
+    }
+  }
+  
   if (argc < 3) { std::cerr<<"usage: "<<argv[0]<<" <n> <nbthreads>"<<std::endl;
     return -1;
   }
 
   int n = atoi(argv[1]);
-
+  int nbthreads = atoi(argv[2]);
+//set number of threads
+  omp_set_num_threads(nbthreads);
   // get arr data
   int * arr = new int [n];
   generateMergeSortData (arr, n);
+//start time
+  std::chrono::time_point<std::chrono::system_clock> start = std::chrono::system_clock::now();
+  for(int i=0; i<n;i++){
 
-  //insert sorting code here.
-  OmpLoop o1;
+    int start = i%2;
 
-  o1.setNbThread(atoi(argv[2]));
-
-  //printArr(std::ref(arr), n);
-
-  for (int i = 0; i < n; i++) {
-
-    int first = i % 2;
-
-    o1.parfor<int>(
-      first, n - 1, 2,
-      [&](int (& tls)){
-        tls = 0;
-      },
-      [&](int i, int (& tls)){
-        if(arr[i] > arr[i + 1])
-          std::swap(arr[i] , arr[i + 1]);
-      },
-      [&](int (& tls)){
-        tls++;
+    #pragma omp parallel for default(none) shared(arr, n, start)
+      for(int j=start; j<n-1; j+=2){
+        if(arr[j]>arr[j+1]){
+          swap(arr[j],arr[j+1]); //swap
+        }
       }
-    );
+
+
   }
 
-  //printArr(std::ref(arr), n);
 
+  
   checkMergeSortResult (arr, n);
-
-  // get runtime
-  auto end = std::chrono::system_clock::now();
-  std::chrono::duration<double> diff = end - start;
-  std::cerr << diff.count() << std::endl;
-
+  
   delete[] arr;
+
+  //end time
+  std::chrono::time_point<std::chrono::system_clock> end = std::chrono::system_clock::now();
+  std::chrono::duration<double> elapsed_seconds = end-start;
+
+  // display time to cerr
+  std::cerr<<elapsed_seconds.count()<<std::endl;
 
   return 0;
 }
