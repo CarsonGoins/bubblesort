a8af0905fce91e84f50298469385ebba75e83fc4
CarsonGoins <71525076+CarsonGoins@users.noreply.github.com>
2021-12-12 13:24:47 -0500

    Update bubblesort.cpp

--- bubblesort/bubblesort.cpp
+++ bubblesort/bubblesort.cpp
@@ -23,16 +23,14 @@ extern "C" {
 
 int main (int argc, char* argv[]) {
 
-
-  //forces openmp to create the threads beforehand
 #pragma omp parallel
   {
-    int fd = open (argv[0], O_RDONLY);
-    if (fd != -1) {
-      close (fd);
+    int x = open (argv[0], O_RDONLY);
+    if (x != -1) {
+      close (x);
     }
     else {
-      std::cerr<<"something is amiss"<<std::endl;
+      std::cerr<<"nope"<<std::endl;
     }
   }
   
@@ -42,38 +40,30 @@ int main (int argc, char* argv[]) {
 
   int n = atoi(argv[1]);
   int nbthreads = atoi(argv[2]);
-//set number of threads
   omp_set_num_threads(nbthreads);
-  // get arr data
   int * arr = new int [n];
   generateMergeSortData (arr, n);
-//start time
   std::chrono::time_point<std::chrono::system_clock> start = std::chrono::system_clock::now();
   for(int i=0; i<n;i++){
 
     int start = i%2;
 
-    #pragma omp parallel for default(none) shared(arr, n, start)
+    #pragma omp parallel
+    for default(none) shared(arr, n, start)
       for(int j=start; j<n-1; j+=2){
         if(arr[j]>arr[j+1]){
-          swap(arr[j],arr[j+1]); //swap
+          swap(arr[j],arr[j+1]);
         }
       }
 
 
   }
-
-
-  
   checkMergeSortResult (arr, n);
-  
   delete[] arr;
-
-  //end time
+  
   std::chrono::time_point<std::chrono::system_clock> end = std::chrono::system_clock::now();
   std::chrono::duration<double> elapsed_seconds = end-start;
 
-  // display time to cerr
   std::cerr<<elapsed_seconds.count()<<std::endl;
 
   return 0;
